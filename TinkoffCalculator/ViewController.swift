4a99cffe9c75c1cdffff03c2c3282dcd53ac0a1e
Lilyaliya <lilyaliya@inbox.ru>
2024-03-18 19:43:21 +0300

    feat(storyboard): calc buttons

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        guard let buttonText = sender.currentTitle else {
+            return
+        }
+        
+        print(buttonText)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print("here it is!")
     }
 
 
